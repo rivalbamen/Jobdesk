d366c98d0ed7cd1659e45520cf8e0c8cc99a9989
kajhi <muhammadmirza25@gmail.com>
2017-02-15 15:25:22 +0800

    coba BoardControler

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -17,6 +17,7 @@ return array(
     'Slim\\' => array($vendorDir . '/slim/slim/Slim'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
+    'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'League\\Plates\\' => array($vendorDir . '/league/plates/src'),
     'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
