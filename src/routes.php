ebab3dbce86ce874394bc796ccfb7945c5c8f0ad
kajhi <muhammadmirza25@gmail.com>
2017-02-15 17:41:58 +0800

    routes tambah

--- src/routes.php
+++ src/routes.php
@@ -38,5 +38,14 @@ $app->group('/user', function() {
 
 });
 
+$app->group('/board', function() {
+	$this->get('/', App\Controller\BoardController::class)->setName('tampil-board');
+	$this->get('/add', App\Controller\BoardController::class.':form')->setName('form-board');
+	// $this->get('/update/{id}', App\Controller\BoardController::class.':form')->setName('update-board');
+	// $this->post('/save', App\Controller\BoardController::class.':save')->setName('save-board');
+	// $this->get('/delete/{id}', App\Controller\BoardController::class.':delete')->setName('delete-board');
+
+});
+
 
 
