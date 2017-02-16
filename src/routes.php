82a2776aa2d6b09909c61b2197eb43cbd7abad08
WawanAndiika <ikadekwawanandika@gmail.com>
2017-02-16 16:40:51 +0800

    routes and board

--- src/routes.php
+++ src/routes.php
@@ -40,7 +40,7 @@ $app->group('/user', function() {
 
 $app->group('/board', function() {
 	$this->get('/', App\Controller\BoardController::class)->setName('tampil-board');
-	$this->get('/add', App\Controller\BoardController::class.':form')->setName('form-board');
+	$this->get('/list/{id}', App\Controller\ListController::class)->setName('list-board');
 	// $this->get('/update/{id}', App\Controller\BoardController::class.':form')->setName('update-board');
 	$this->post('/save', App\Controller\BoardController::class.':save')->setName('save-board');
 	// $this->get('/delete/{id}', App\Controller\BoardController::class.':delete')->setName('delete-board');
